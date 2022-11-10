8d90aaa8965e6e63731931f7f3f0e0b4f7fa0e02
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-11-10 04:47:45 +0300

    CoinFlip

---
+++ 3. Coin Flip/deploy_coinflip.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const CoinFlip = await hre.ethers.getContractFactory("CoinFlip");
+  const coinFlip = await CoinFlip.deploy();
+  await coinFlip.deployed();
+  console.log(`Fallback deployed to ${coinFlip.address}`);
+
+  const Hack = await hre.ethers.getContractFactory("Hack");
+  const hack = await Hack.deploy(coinFlip.address);
+  await hack.deployed();
+  console.log(`Fallback deployed to ${hack.address}`);
+
+  for(i = 0; i < 10; ++i) {
+    hack.attack();
+    console.log(await coinFlip.consecutiveWins())
+  }
+
+  console.log(await ethers.provider.getStorageAt(coinFlip.address, 0))
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
