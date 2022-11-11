cd26aa4f1fe4c91a1385e813b6887e22ef80397f
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-11-11 15:07:10 +0300

    GatekeeperTwo

---
+++ 14. Gatekeeper Two/deploy_gatekeeperTwo.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const [owner] = await ethers.getSigners();
+
+  const GatekeeperTwo = await hre.ethers.getContractFactory("GatekeeperTwo");
+  const gatekeeperTwo = await GatekeeperTwo.deploy(); 
+  await gatekeeperTwo.deployed();
+  console.log(`GatekeeperTwo deployed to ${gatekeeperTwo.address}`);
+
+  const HackGates = await hre.ethers.getContractFactory("HackGates");
+  const hackGates = await HackGates.deploy(gatekeeperTwo.address); 
+  await hackGates.deployed();
+  console.log(`HackGates deployed to ${hackGates.address}`);
+
+  if (owner.address == await gatekeeperTwo.entrant()) console.log("Done!");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+
